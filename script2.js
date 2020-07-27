d879a4bd26378c4b0a6502c88b88a206eb542a33
chris <chrisshaohualiu@gmail.com>
2020-07-27 15:57:13 -0400

    made a function for extended challenge

--- script2.js
+++ script2.js
@@ -1,8 +1,10 @@
-let hours = 50;
-let wage = 15;
-let paycheck;
-let weeksToAMillion;
+let hours = 10;
+let wage = 20;
 
+
+function paycheckCalculator(wage, hours){
+    let paycheck;
+    let weeksToAMillion;
 if(hours<=40){
     paycheck = wage*hours;
 }
@@ -10,6 +12,13 @@ else if(hours>40){
     paycheck = (40+(hours-40)*1.5)*wage;
 }
 
-console.log(`${paycheck} dollars made this week.`)
-weeksToAMillion = Math.floor(1000000/paycheck);
-console.log(`${weeksToAMillion} until a million dollars made.`);
+
+console.log(`If our wage is $${wage}/hr and we worked ${hours} hours then our paycheck is $${paycheck}.`)
+weeksToAMillion = Math.ceil(1000000/paycheck);
+console.log(`${weeksToAMillion} weeks until a million dollars made.`);
+}
+
+paycheckCalculator(10, 20);
+paycheckCalculator(10, 40);
+paycheckCalculator(10, 50);
+paycheckCalculator(12, 60);
